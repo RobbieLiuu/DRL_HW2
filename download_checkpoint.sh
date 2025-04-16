7d8dbcd736f76aef2d70d5557f0169a32db51f6b
RobbieLiuu <robbie0321132@gmail.com>
2025-04-16 11:19:13 +0800

    new model

--- download_checkpoint.sh
+++ download_checkpoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install --upgrade gdown
-gdown "https://drive.google.com/uc?id=1pjXQ0zy2JEGdROrOV17PCMnOzHThrN0Y&export=download"
+gdown "https://drive.google.com/uc?id=1fyBEE0zTtpmIfvaMXFkSjjaS0SXRcPAY&export=download"
