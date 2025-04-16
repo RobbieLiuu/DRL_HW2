deb49b91d6147d7e550071dcf870e88527791de7
RobbieLiuu <robbie0321132@gmail.com>
2025-04-16 11:48:23 +0800

    .sh fix

--- download_checkpoint.sh
+++ download_checkpoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install --upgrade gdown
-gdown --fuzzy "https://drive.google.com/file/d/1fyBEE0zTtpmIfvaMXFkSjjaS0SXRcPAY/view?usp=drive_link"
+gdown --fuzzy "https://drive.google.com/file/d/1UxZq6Z4Kvj6d6O7PCvTxPnyyIKjtkpum/view?usp=drive_link"
